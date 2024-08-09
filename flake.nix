21a75d67b127bda1b32c116325db517c83a50f6d
laged <parkkila.matti@gmail.com>
2024-08-09 12:31:05 +0300

    feat: flake inputs

--- flake.nix
+++ flake.nix
@@ -2,13 +2,16 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    flakebox = {
+      url = "github:rustshop/flakebox";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-
+  outputs = { self, nixpkgs, flakebox }: {
+    
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
   };
