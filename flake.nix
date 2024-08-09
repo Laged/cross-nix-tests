462e7d10ac8e2ee9713ecf4d981b269417466a6b
laged <parkkila.matti@gmail.com>
2024-08-09 12:55:04 +0300

    feat: flakebox building

--- flake.nix
+++ flake.nix
@@ -11,14 +11,44 @@
   };
 
   outputs = { self, nixpkgs, flakebox, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: 
-      let
-        flakeboxLib = flakebox.lib.${system} {};
-      in
-      {
-        devShells = flakeboxLib.mkShells {
-          packages = [ ];
+     flake-utils.lib.eachDefaultSystem (system:
+       let
+         flakeboxLib = flakebox.lib.${system} { };
+
+        rustSrc = flakeboxLib.filterSubPaths {
+          root = builtins.path {
+            name = "cross-nix-tests";
+            path = ./.;
+          };
+          paths = [
+            "Cargo.toml"
+            "Cargo.lock"
+            ".cargo"
+            "src"
+          ];
         };
-      }
+
+        legacyPackages = (flakeboxLib.craneMultiBuild { }) (craneLib':
+          let
+            craneLib = (craneLib'.overrideArgs {
+              pname = "cross-nix-tests";
+              src = rustSrc;
+            });
+          in
+          rec {
+            workspaceDeps = craneLib.buildWorkspaceDepsOnly { };
+            workspaceBuild = craneLib.buildWorkspace {
+              cargoArtifacts = workspaceDeps;
+            };
+            cross-nix-tests = craneLib.buildPackage { };
+          });
+       in
+       {
+        inherit legacyPackages;
+        packages.default = legacyPackages.cross-nix-tests;
+         devShells = flakeboxLib.mkShells {
+           packages = [ ];
+         };
+        }
     );
 }
